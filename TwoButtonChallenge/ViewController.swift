46fc75122906450b4470eb6bc9ae8e1ff55b080a
Christopher Greene <greenecr@bc.edu>
2020-01-14 22:22:12 -0500

    Two Button Challenge first attempt

--- TwoButtonChallenge/ViewController.swift
+++ TwoButtonChallenge/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func message1ButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Awesome!"
+        print("First message button has been pressed")
+    }
+    
+    @IBAction func message2ButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Great!"
+        print("Second message button has been pressed")
+    }
 }
 
